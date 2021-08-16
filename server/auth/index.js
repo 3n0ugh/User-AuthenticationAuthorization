6f1ecaf36d65cb58cf8b2832e083aab8ee3d5723
3n0ugh <my@emailaddress.com>
2021-08-16 12:03:05 +0000

    :sparkles: Login Route Adding

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,61 +1,112 @@
-const express = require("express");
-const Joi = require("joi");
-const bcrypt = require("bcryptjs");
+const express = require('express');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const db = require("../db/connection");
-const users = db.get("users");
-users.createIndex("username", { unique: true });
+const db = require('../db/connection');
+const users = db.get('users');
+users.createIndex('username', { unique: true });
 
 const router = express.Router();
 
 const schema = Joi.object({
-	username: Joi.string().alphanum().min(2).max(30).required(),
+  username: Joi.string().alphanum().min(2).max(30).required(),
 
-	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).trim(),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).trim(),
 
-	repeat_password: Joi.ref("password"),
+  repeat_password: Joi.ref('password'),
 });
 
-router.get("/", (req, res) => {
-	res.json({
-		message: "Success",
-	}).status(200);
+router.get('/', (req, res) => {
+  res
+    .json({
+      message: 'Success',
+    })
+    .status(200);
 });
 
-router.post("/signup", (req, res, next) => {
-	const result = schema.validate(req.body);
-	if (!result.hasOwnProperty("error")) {
-		users
-			.findOne({
-				username: req.body.username,
-			})
-			.then((user) => {
-				if (user) {
-					const error = new Error(
-						"That user name is already taken. Please choose another one."
-					);
-					res.status(409);
-					next(error);
-				} else {
-					bcrypt
-						.hash(req.body.password.trim(), 12)
-						.then((hashedPassword) => {
-							const newUser = {
-								username: req.body.username,
-								password: hashedPassword,
-							};
+router.post('/signup', (req, res, next) => {
+  const result = schema.validate(req.body);
+  if (!result.hasOwnProperty('error')) {
+    users
+      .findOne({
+        username: req.body.username,
+      })
+      .then((user) => {
+        if (user) {
+          const error = new Error(
+            'That user name is already taken. Please choose another one.'
+          );
+          res.status(409);
+          next(error);
+        } else {
+          bcrypt.hash(req.body.password.trim(), 12).then((hashedPassword) => {
+            const newUser = {
+              username: req.body.username,
+              password: hashedPassword,
+            };
 
-							users.insert(newUser).then((insertedUser) => {
-								delete insertedUser.password;
-								res.json({ insertedUser });
-							});
-						});
-				}
-			});
-	} else {
-		res.status(422);
-		next(result.error);
-	}
+            users.insert(newUser).then((insertedUser) => {
+              delete insertedUser.password;
+              res.json({ insertedUser });
+            });
+          });
+        }
+      });
+  } else {
+    res.status(422);
+    next(result.error);
+  }
+});
+
+function respondError422(res, next) {
+  res.status(422);
+  const error = new Error('Unable to login.');
+  next(error);
+}
+
+router.post('/login', (req, res, next) => {
+  const result = schema.validate(req.body);
+  if (!result.hasOwnProperty('error')) {
+    users
+      .findOne({
+        username: req.body.username,
+      })
+      .then((user) => {
+        if (user) {
+          bcrypt.compare(req.body.password, user.password).then((result) => {
+            if (result) {
+              const payload = {
+                _id: user._id,
+                username: user.username,
+              };
+              jwt.sign(
+                payload,
+                process.env.TOKEN_SECRET,
+                {
+                  expiresIn: '1 days',
+                },
+                (err, token) => {
+                  if (err) {
+                    respondError422(res, next);
+                  } else {
+                    res.json({
+                      token,
+                    });
+                  }
+                }
+              );
+            } else {
+              respondError422(res, next);
+            }
+          });
+        } else {
+          respondError422(res, next);
+        }
+      });
+  } else {
+    respondError422(res, next);
+  }
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,37 +1,41 @@
-const express = require("express");
-const volleyball = require("volleyball");
-const cors = require("cors");
+const express = require('express');
+const volleyball = require('volleyball');
+const cors = require('cors');
 
-const auth = require("./auth");
+require('dotenv').config();
+
+const auth = require('./auth');
 
 const app = express();
 
 app.use(volleyball);
-app.use(cors({
-	origin: 'http://localhost:8080'
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+  })
+);
 app.use(express.json());
 
-app.get("/", (req, res) => {
-	res.json({
-		message: "Hello World !!",
-	});
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World !!',
+  });
 });
 
-app.use("/auth", auth);
+app.use('/auth', auth);
 
 function notFound(req, res, next) {
-	res.status(404);
-	const error = new Error("Not Found - " + req.originalUrl);
-	next(error);
+  res.status(404);
+  const error = new Error('Not Found - ' + req.originalUrl);
+  next(error);
 }
 
 function errorHandler(err, req, res, next) {
-	res.status(res.statusCode || 500);
-	res.json({
-		message: err.message,
-		stack: err.stack,
-	});
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: err.stack,
+  });
 }
 
 app.use(notFound);
@@ -40,5 +44,5 @@ app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-	console.log(`Server listening on ${port}...`);
+  console.log(`Server listening on ${port}...`);
 });
